caec0f7e95c41dae58d26471b9d120e9a5050f5b
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2023-04-17 22:44:28 +0200

    Better js handling for multple codeowners

--- .github/workflows/scripts/code_owners_comment.js
+++ .github/workflows/scripts/code_owners_comment.js
@@ -24,15 +24,18 @@ function findCodeOwners(projectName) {
   console.log(codeOwners)
   for (const line of codeOwners) {
     if (line === '' || line.indexOf('#') === 0) continue
-    console.log(`Line: ${line}`)
-    const [pattern, owners] = line.split(/\s+/)
-    console.log(pattern)
-    console.log(owners)
-    const regex = new RegExp('^' + pattern.replace(/[.*+?^${}()|[\]\\]/g, '\\$&').replace(/\/\*\*/g, '(?:\/[^\/]+)*\/?'));
-
-    if (regex.test(projectName)) {
-      return owners.split(/\s+/)
+    const matchingOwners = [];
+    for (const line of codeOwners) {
+      const [pattern, owners] = line.split(/\s+/);
+  
+      // Escape special characters in the pattern and replace * with a regular expression that matches any number of characters except /.
+      const regex = new RegExp('^' + pattern.replace(/[.*+?^${}()|[\]\\]/g, '\\$&').replace(/\/\*\*/g, '(?:\/[^\/]+)*\/?'));
+      if (regex.test(projectPath)) {
+        matchingOwners.push(...owners.split(/\s+/));
+      }
     }
+  
+    return matchingOwners;
   }
 
   return null
@@ -42,11 +45,11 @@ module.exports = async ({ github, context, core }) => {
   const pullRequestTitle = context.payload.pull_request.title
   if (pullRequestTitle.indexOf('[') !== 0) return
   const projectName = findProjectName(pullRequestTitle)
-  // if (projectName == null) throw Error("Project name not found! No folder under /projects/[your-project-name]")
+  if (!projectName) throw Error("Project name not found! No folder under /projects/[your-project-name]")
 
   const codeOwners = findCodeOwners(projectName)
   console.log(codeOwners)
-  if (codeOwners == null) return
+  if (codeOwners.length == 0) return
 
   const body = `${codeOwners.join(', ')} can you please review this pull-request? You have been assigned as code-owner to this project`
   github.rest.issues.createComment({
