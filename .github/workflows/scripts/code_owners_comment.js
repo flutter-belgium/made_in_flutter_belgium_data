f84791e6f4904ca2bcb3193e4ccb951e51ec7a18
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2023-04-17 22:03:27 +0200

    read codeowners

--- .github/workflows/scripts/code_owners_comment.js
+++ .github/workflows/scripts/code_owners_comment.js
@@ -1,10 +1,17 @@
 module.exports = async ({ github, context, core }) => {
-    const codeOwners = ["@vanlooverenkoen", "@vanlooverenkoen"]
-    const body = `${codeOwners.join(', ')} can you please review this pull-request? You have been assigned as code-owner to this project`
-    github.rest.issues.createComment({
-      issue_number: context.issue.number,
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      body: body
-    })
+  const fs = require('fs')
+  const path = require('path')
+  const filePath = path.join(__dirname, 'CODEOWNERS.md')
+
+  const data = fs.readFileSync(filePath, { encoding: 'utf-8' })
+  console.log('received data: ' + data);
+
+  const codeOwners = ["@vanlooverenkoen", "@vanlooverenkoen"]
+  const body = `${codeOwners.join(', ')} can you please review this pull-request? You have been assigned as code-owner to this project`
+  github.rest.issues.createComment({
+    issue_number: context.issue.number,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    body: body
+  })
 }
