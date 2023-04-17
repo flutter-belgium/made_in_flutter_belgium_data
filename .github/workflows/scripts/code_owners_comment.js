213f1d7749b012455b5d47101df9efdf452d21c7
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2023-04-17 22:01:11 +0200

    Fixed v3 to v6 migration

--- .github/workflows/scripts/code_owners_comment.js
+++ .github/workflows/scripts/code_owners_comment.js
@@ -1,7 +1,7 @@
 module.exports = async ({ github, context, core }) => {
     const codeOwners = ["@vanlooverenkoen", "@vanlooverenkoen"]
     const body = `${codeOwners.join(', ')} can you please review this pull-request? You have been assigned as code-owner to this project`
-    github.issues.createComment({
+    github.rest.issues.createComment({
       issue_number: context.issue.number,
       owner: context.repo.owner,
       repo: context.repo.repo,
