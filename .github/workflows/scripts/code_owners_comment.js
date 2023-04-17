5cb89219d0ba1bd02a6b8b1b753ac891c6cea533
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2023-04-17 21:51:39 +0200

    Moved ci to seperate scripts

---
+++ .github/workflows/scripts/code_owners_comment.js
@@ -0,0 +1,10 @@
+module.exports = async ({ github, context, core }) => {
+    const codeOwners = ["@vanlooverenkoen", "@vanlooverenkoen"]
+    const body = `${codeOwners.join(', ')} can you please review this pull-request? You have been assigned as code-owner to this project`
+    github.issues.createComment({
+      issue_number: context.issue.number,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      body: body
+    })
+}
