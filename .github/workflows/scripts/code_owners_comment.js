3ee44396dcd1885645723e345d9f84462f65b381
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2023-04-17 22:15:46 +0200

    Updated the code owners comment

--- .github/workflows/scripts/code_owners_comment.js
+++ .github/workflows/scripts/code_owners_comment.js
@@ -1,12 +1,39 @@
-module.exports = async ({ github, context, core }) => {
-  const fs = require('fs')
-  const path = require('path')
+const fs = require('fs')
+const path = require('path')
+
+
+function findProjectPath(pullRequestTitle) {
+  const projectName = pullRequestTitle.match(/\[(.*?)\]/)[1]
+  const projectFolder = path.join(`projects/${projectName}`)
+
+  if (!fs.existsSync(projectFolder)) return null
+  return projectName
+}
+
+function findCodeOwners(projectPath) {
   const filePath = path.join('CODEOWNERS')
-
   const data = fs.readFileSync(filePath, { encoding: 'utf-8' })
-  console.log('received data: ' + data);
 
-  const codeOwners = ["@vanlooverenkoen", "@vanlooverenkoen"]
+  const codeOwners = data.split('\n')
+  for (const line of codeOwners) {
+    const [pattern, owners] = line.split(/\s+/)
+    const regex = new RegExp(pattern);
+
+    if (regex.test(projectPath)) {
+      return owners.split(/\s+/)
+    }
+  }
+
+  return null
+}
+
+module.exports = async ({ github, context, core }) => {
+  const projectName = findProjectPath(context.payload.pull_request.title)
+  if (projectName == null) throw Error("Project name not found! No folder under /projects/[your-project-name]")
+
+  const codeOwners = findCodeOwners(projectName)
+  if (codeOwners == null) return
+
   const body = `${codeOwners.join(', ')} can you please review this pull-request? You have been assigned as code-owner to this project`
   github.rest.issues.createComment({
     issue_number: context.issue.number,
