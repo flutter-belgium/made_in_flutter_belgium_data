80aab923000ddf939d7df072f7f03b4e1f0d2685
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2023-04-25 21:11:18 +0200

    Test to update the repository

--- .github/workflows/scripts/dart/api_validate/lib/api_validate.dart
+++ .github/workflows/scripts/dart/api_validate/lib/api_validate.dart
@@ -5,7 +5,7 @@ import 'package:api_validate/utils/validate_projects.dart';
 import 'package:path/path.dart';
 
 Future<void> apiValidate() async {
-  final workingDir = join('..', '..', '..', '..', '..', Directory.current.path);
+  final workingDir = join('..', '..', '..', '..', Directory.current.path);
   await validateCompanies(workingDir);
   await validateProjects(workingDir);
 }
