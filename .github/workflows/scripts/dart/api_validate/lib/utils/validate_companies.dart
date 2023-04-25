a23c13dd7b2e8ae4f2f9e7e5e3296c4eb14f3a01
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2023-04-25 21:47:21 +0200

    Simple validation

--- .github/workflows/scripts/dart/api_validate/lib/utils/validate_companies.dart
+++ .github/workflows/scripts/dart/api_validate/lib/utils/validate_companies.dart
@@ -1,4 +1,20 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:made_in_flutter_belgium_data/made_in_flutter_belgium_data.dart';
+
 Future<void> validateCompanies(String workingDirPath) async {
-  // final companiesDir = Directory(join(workingDirPath, 'companies'));
-  // if (!companiesDir.existsSync()) throw ArgumentError('companies directory not found');
+  final companiesDirectory = Directory(join(workingDirPath, 'companies'));
+  if (!companiesDirectory.existsSync()) throw ArgumentError('companies directory not found');
+  for (final dir in companiesDirectory.listSync()) {
+    if (dir is! Directory) continue;
+    final dirSegments = dir.uri.pathSegments;
+    final companyName = dirSegments[dirSegments.length - 1];
+    print('Company: $companyName start validation');
+    final infoJsonFile = File(join(dir.path, 'info.json'));
+    final infoJsonString = await infoJsonFile.readAsString();
+    Company.fromJson(jsonDecode(infoJsonString) as Map<String, dynamic>);
+    print('Company: $companyName has a valid info.json 💙💙!');
+  }
 }
