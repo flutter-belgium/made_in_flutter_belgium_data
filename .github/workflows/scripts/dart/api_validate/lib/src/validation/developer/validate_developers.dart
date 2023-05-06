8e1bfd9b937acff80586ad16a2d8302545fae7f7
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2023-05-06 15:42:17 +0200

    #57: Fixed a bug where not all developers were exported

--- .github/workflows/scripts/dart/api_validate/lib/src/validation/developer/validate_developers.dart
+++ .github/workflows/scripts/dart/api_validate/lib/src/validation/developer/validate_developers.dart
@@ -5,10 +5,11 @@ import 'package:api_validate/src/validation/developer/validate_developer_images.
 import 'package:api_validate/src/validation/developer/validate_developer_links.dart';
 import 'package:api_validate/src/validation/validate_dir.dart';
 import 'package:api_validate/src/writor/list_writor.dart';
+import 'package:collection/collection.dart';
 import 'package:made_in_flutter_belgium_data/made_in_flutter_belgium_data.dart';
 import 'package:path/path.dart';
 
-Future<List<Developer>> validateDevelopers(String workingDirPath) async {
+Future<List<Developer>> validateDevelopers(List<Project> projectDevelopers, String workingDirPath) async {
   final developers = await validateDir(
     workingDirPath,
     'developers',
@@ -22,21 +23,37 @@ Future<List<Developer>> validateDevelopers(String workingDirPath) async {
           'Check the documentation for more information. https://github.com/flutter-belgium/made_in_flutter_belgium_data/tree/main/examples/developers',
         );
       }
-      await validateDeveloperImages(developer, workingDirPath, itemDir);
-      await validateDeveloperLinks(developer);
+      await _updateDeveloper(developer, workingDirPath, itemDir);
       return developer;
     },
   );
-  return developers..sort((a, b) => a.name!.toLowerCase().compareTo(b.name!.toLowerCase()));
+  await _addMissingDevelopers(developers, projectDevelopers, workingDirPath);
+  return developers..sort((a, b) => a.githubUserName.toLowerCase().compareTo(b.githubUserName.toLowerCase()));
+}
+
+Future<void> _updateDeveloper(Developer developer, String workingDirPath, Directory itemDir) async {
+  await validateDeveloperImages(developer, workingDirPath, itemDir);
+  await validateDeveloperLinks(developer);
+}
+
+Future<void> _addMissingDevelopers(List<Developer> developers, List<Project> projects, String workingDirPath) async {
+  final projectDevelopers = projects.expand<MinimizedDeveloper>((map) => map.developers ?? []).toList();
+  print(projectDevelopers.length);
+  for (final projectDeveloper in projectDevelopers) {
+    final existingDeveloper = developers.firstWhereOrNull((developer) => developer.githubUserName == projectDeveloper.githubUserName);
+    if (existingDeveloper == null) {
+      final developer = Developer(githubUserName: projectDeveloper.githubUserName);
+      await _updateDeveloper(developer, workingDirPath, Directory(join(workingDirPath, 'api', 'developers', developer.githubUserName)));
+      developers.add(developer);
+    }
+  }
 }
 
 Future<void> saveDevelopersToApi(List<Developer> developers, String workingDirPath) async {
   final dir = join('api', 'developers');
   final developersApiDir = Directory(join(workingDirPath, dir));
-  if (!developersApiDir.existsSync()) {
-    developersApiDir.createSync(recursive: true);
-  }
   for (final developer in developers) {
+    print(developer.githubUserName);
     final developerPath = join(developersApiDir.path, developer.githubUserName);
     final developerDir = Directory(developerPath);
     if (!developerDir.existsSync()) {
