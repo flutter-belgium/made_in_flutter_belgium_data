d9b02100534483a5c1caf8730d7b0388020d3874
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2023-05-24 17:11:39 +0200

    Fixed analyzer

--- .github/workflows/scripts/dart/api_deploy/lib/api_deploy.dart
+++ .github/workflows/scripts/dart/api_deploy/lib/api_deploy.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
-import 'package:api_deploy/src/diff_util.dart';
-import 'package:api_deploy/src/repo/api_repository.dart';
+// import 'package:api_deploy/src/diff_util.dart';
+// import 'package:api_deploy/src/repo/api_repository.dart';
 import 'package:api_deploy/src/writor/build_api_dir.dart';
 import 'package:api_validate/api_validate.dart';
 import 'package:path/path.dart';
